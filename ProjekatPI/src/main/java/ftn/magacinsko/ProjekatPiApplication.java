137ee7a550a97d9c3c46f492c5cc1539c29a7004
maricm123 <mihailomaric001@gmail.com>
2021-08-31 11:23:10 +0200

    Fixed some hibernate and database issues

--- ProjekatPI/src/main/java/ftn/magacinsko/ProjekatPiApplication.java
+++ ProjekatPI/src/main/java/ftn/magacinsko/ProjekatPiApplication.java
@@ -1,4 +1,5 @@
-package ftn.magacinsko.ProjekatPI;
+package ftn.magacinsko;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
